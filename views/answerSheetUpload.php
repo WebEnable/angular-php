5930e60ffdfb78b2f3b197e0547bdd8f3834cd31
Ranganath Vishwanath <ranganath.vishwanath@levitum.com>
2014-03-12 11:46:38 +0530

    changes

--- views/answerSheetUpload.php
+++ views/answerSheetUpload.php
@@ -27,12 +27,12 @@
 		    <p class="help-block">Please upload pdf files</p>
 		  </div>
 
-		  <p class="input-group">
+		  <div class="input-group">
               <input type="text" class="form-control input-lg" datepicker-popup="{{format}}" ng-model="dt" is-open="opened" min="" max="minDate" datepicker-options="dateOptions" date-disabled="disabled(date, mode)" ng-required="true" close-text="Close" />
               <span class="input-group-btn">
                 <button class="btn btn-default" ng-click="open($event)"><i class="glyphicon glyphicon-calendar"></i></button>
               </span>
-            </p>
+          </div>
 		  <button type="submit" class="btn-primary btn-lg">Add</button>
 		</form>
 	</div>
