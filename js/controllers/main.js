99e92b6da369e7c6da37c2a1a1c6ca20ace362a1
Ranganath Vishwanath <ranganath.vishwanath@levitum.com>
2014-03-12 18:41:07 +0530

    updated

--- js/controllers/main.js
+++ js/controllers/main.js
@@ -5,9 +5,9 @@ angular.module('NileshTutorial')
 
     $scope.UserService = UserService;
 
-    if($scope.UserService.isLogged){
+/*    if($scope.UserService.isLogged){
       $location.path('/admin');}
-      else{
+      else*/{
         $scope.loginData = {};
         $scope.login = function() {
         $http({
@@ -68,17 +68,61 @@ angular.module('NileshTutorial')
       /*}*/
   });
 angular.module('NileshTutorial')
-  .controller('addTestCntrl', function ($scope) {
+  .controller('addTestCntrl', function ($scope,$location,UserService,ClassSubject) {
+    $scope.UserService = UserService;
+    $scope.ClassSubject= ClassSubject;
+    $scope.testData = {};
+
+        $scope.today = function() {
+          $scope.testData.dt = new Date();
+        };
+        $scope.today();
+
+        $scope.showWeeks = true;
+        $scope.toggleWeeks = function () {
+          $scope.showWeeks = ! $scope.showWeeks;
+        };
+
+        $scope.clear = function () {
+          $scope.testData.dt = null;
+        };
+        // Disable weekend selection
+        $scope.disabled = function(date, mode) {
+          return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+        };
+
+        $scope.toggleMin = function() {
+          $scope.minDate = ( $scope.minDate ) ? null : new Date();
+        };
+        $scope.toggleMin();
+
+        $scope.open = function($event) {
+          $event.preventDefault();
+          $event.stopPropagation();
+          $scope.opened = true;
+        };
+
+        $scope.dateOptions = {
+          'year-format': "'yy'",
+          'starting-day': 1
+        };
+
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy-MM-dd', 'shortDate'];
+        $scope.format = $scope.formats[1];
+        $scope.classes = $scope.ClassSubject.classes[0];
+        $scope.subjects = $scope.ClassSubject.Subjects[0];
+
   });
 angular.module('NileshTutorial')
   .controller('addTestMarks', function ($scope) {
   });
 
 angular.module('NileshTutorial')
-  .controller('uploadAnswerSheet', function ($scope,$location,UserService,ClassSubject) {
+  .controller('uploadAnswerSheet', function ($scope,$location,UserService,ClassSubject,$routeParams) {
 
     $scope.UserService = UserService;
     $scope.ClassSubject= ClassSubject;
+    $scope.routeParams = $routeParams;
 
         $scope.today = function() {
           $scope.dt = new Date();
@@ -93,7 +137,6 @@ angular.module('NileshTutorial')
         $scope.clear = function () {
           $scope.dt = null;
         };
-
         // Disable weekend selection
         $scope.disabled = function(date, mode) {
           return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
@@ -107,7 +150,6 @@ angular.module('NileshTutorial')
         $scope.open = function($event) {
           $event.preventDefault();
           $event.stopPropagation();
-
           $scope.opened = true;
         };
 
@@ -116,20 +158,61 @@ angular.module('NileshTutorial')
           'starting-day': 1
         };
 
-        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'shortDate'];
-        $scope.format = $scope.formats[0];
+        $scope.formats = ['dd-MMMM-yyyy', 'yyyy-MM-dd', 'shortDate'];
+        $scope.format = $scope.formats[1];
         $scope.classes = $scope.ClassSubject.classes[0];
         $scope.subjects = $scope.ClassSubject.Subjects[0];
   });
 
 angular.module('NileshTutorial')
-  .controller('AnswerSheetList', function ($scope,$location,UserService,ClassSubject) {
+  .controller('AnswerSheetList', function ($scope,$http,$location,UserService,ClassSubject) {
 
     $scope.UserService = UserService;
+    $scope.currentPageA = 0;
+    $scope.pageSizeA = 50;
+//    $scope.data = [];
+    $scope.numberOfPages=function(){
+        return Math.ceil($scope.data.length/$scope.pageSize);                
+    }
+
+    $http.get('json/answerSheetList.php').success(function(data) { 
+      $scope.answerSheetList = data;
+    });
+
+    $scope.numberOfPagesA=function(){
+        return Math.ceil($scope.answerSheetList.length/$scope.pageSizeA);                
+    }
+
 
 /**/
   });
   
+angular.module('NileshTutorial')
+  .controller('hdrCntrl', function ($scope,UserService) {
+      $scope.UserService = UserService;
+});
+
+angular.module('NileshTutorial')
+  .controller('studentListCtrl', function ($scope,$http,UserService) {
+    $scope.UserService = UserService;
+    $scope.currentPage = 0;
+    $scope.pageSize = 100;
+//    $scope.data = [];
+    $scope.numberOfPages=function(){
+        return Math.ceil($scope.data.length/$scope.pageSize);                
+    }
+
+    $http.get('json/studentList.php').success(function(data) { 
+      $scope.studentList = data;
+    });
+
+    $scope.numberOfPages=function(){
+        return Math.ceil($scope.studentList.length/$scope.pageSize);                
+    }
+
+});
+
+
 angular.module('NileshTutorial')
   .controller('logoutCntrl', function ($scope,$http,$location,UserService) {
     $scope.logoutData = "logout";
@@ -148,37 +231,12 @@ angular.module('NileshTutorial')
             }
         });
 });
-angular.module('NileshTutorial')
-  .controller('hdrCntrl', function ($scope,UserService) {
-      $scope.UserService = UserService;
-});
 
-angular.module('NileshTutorial')
-  .controller('studentListCtrl', function ($scope,$http,UserService) {
-    $scope.UserService = UserService;
-    $scope.currentPage = 0;
-    $scope.pageSize = 3;
-//    $scope.data = [];
-    $scope.numberOfPages=function(){
-        return Math.ceil($scope.data.length/$scope.pageSize);                
-    }
-/*    for (var i=0; i<45; i++) {
-        $scope.data.push("Item "+i);
-    }*/
 
-    $http.get('json/studentList.php').success(function(data) { 
-      $scope.studentList = data;
-    });
-
-    $scope.numberOfPages=function(){
-        return Math.ceil($scope.studentList.length/$scope.pageSize);                
-    }
-
-});
 angular.module('NileshTutorial')
 .filter('startFrom', function() {
     return function(input, start) {
-        start = +start; //parse to int
+        start = +parseInt(start); //parse to int
         return input.slice(start);
     }
 });
