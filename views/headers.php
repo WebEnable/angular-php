434ea90a09f759ffb4ae8176d8cd409482de8421
Ranganath Vishwanath <ranganath.vishwanath@levitum.com>
2014-03-12 18:40:11 +0530

    header updated

--- views/headers.php
+++ views/headers.php
@@ -15,21 +15,20 @@
       <ul class="nav navbar-nav" ng-if="UserService.isLogged">
       <li class="">
           <a href="#/admin">Home</a></li>
-<!--    <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li> -->
+
       </ul>
 <!-- {{UserService.isLogged}} -->
       <ul class="nav navbar-nav navbar-right">
+        
+        <li class="dropdown" ng-if="UserService.isLogged">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-cog"></span><b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Change Password</a></li>
+            <li class="divider"></li>
+            <li><a href="#"></a></li>
+          </ul>
+        </li>
+
         <li ng-if="UserService.isLogged"><a href="#/logout">Logout</a></li>
         <li ng-if="!UserService.isLogged"><a href="#/">Login</a></li>
       </ul>
